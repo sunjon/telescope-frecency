78e688e07e8d30ec403b60634bcd89b6099cc213
Tami <tamibam@yahoo.com>
2021-01-15 11:35:22 +0300

    refactor: minor refactoring of sql_wrapper

--- lua/telescope/_extensions/frecency/sql_wrapper.lua
+++ lua/telescope/_extensions/frecency/sql_wrapper.lua
@@ -28,14 +28,14 @@ local schemas = {[[
 ]]}
 
 local queries = {
-  ["file_add_entry"]              = "INSERT INTO files (path, count) values(:path, 1);",
-  ["file_update_counter"]         = "UPDATE files SET count = count + 1 WHERE path = :path;",
-  ["timestamp_add_entry"]         = "INSERT INTO timestamps (file_id, timestamp) values(:file_id, julianday('now'));",
-  ["timestamp_delete_before_id"]  = "DELETE FROM timestamps WHERE id < :id and file_id == :file_id;",
+  ["file_add_entry"]              = "INSERT INTO files (path, count) values(?, 1);",
+  ["file_update_counter"]         = "UPDATE files SET count = count + 1 WHERE path = ?;",
+  ["timestamp_add_entry"]         = "INSERT INTO timestamps (file_id, timestamp) values(?, julianday('now'));",
+  ["timestamp_delete_before_id"]  = "DELETE FROM timestamps WHERE id < ? and file_id == ?;",
   ["get_all_filepaths"]           = "SELECT * FROM files;",
   ["get_all_timestamp_ages"]      = "SELECT id, file_id, CAST((julianday('now') - julianday(timestamp)) * 24 * 60 AS INTEGER) AS age FROM timestamps;",
-  ["get_row"]                     = "SELECT * FROM files WHERE path == :path;",
-  ["get_timestamp_ids_for_file"]  = "SELECT id FROM timestamps WHERE file_id == :file_id;",
+  ["get_row"]                     = "SELECT * FROM files WHERE path == ?;",
+  ["get_timestamp_ids_for_file"]  = "SELECT id FROM timestamps WHERE file_id == ?;",
 }
 
 -- local ignore_patterns = {
@@ -73,21 +73,20 @@ function M:bootstrap(opts)
 
   self.max_entries = opts.max_entries or 2000
 
-  -- create the db if it doesn't exist
+  -- create the db object if it doesn't exist
   local db_root = opts.docs_root or "$XDG_DATA_HOME/nvim"
   local db_filename = db_root .. "/file_frecency.sqlite3"
-  self.db = sql.open(db_filename)
-  if not self.db then
-    print("error")
-    return
-  end
+  self.db = sql.new(db_filename)
+  if not self.db then print("error") return end
 
   -- create tables if they don't exist
-  for _, s in pairs(schemas) do
-    self.db:eval(s)
+  if vim.loop.fs_stat(db_filename) == nil then -- make sense?
+    return self.db:with_open(function(db)
+      for _, s in pairs(schemas) do
+        db:eval(s)
+      end
+    end)
   end
-  self.db:close()
-
 end
 
 function M:do_transaction(query, params)
@@ -96,13 +95,13 @@ function M:do_transaction(query, params)
     return
   end
 
-  local res
-  self.db:with_open(function(db) res = db:eval(queries[query], params) end)
-  return res
+  return self.db:with_open(function(db)
+    return db:eval(queries[query], params)
+  end)
 end
 
 function M:get_row_id(filepath)
-  local result = self:do_transaction('get_row', { path = filepath })
+  local result = self:do_transaction('get_row', filepath)
 
   return type(result) == "table" and result[1].id or nil
 end
@@ -118,21 +117,21 @@ function M:update(filepath)
   local file_id
   file_id = self:get_row_id(filepath)
   if not file_id then
-    self:do_transaction('file_add_entry', { path = filepath })
+    self:do_transaction('file_add_entry', filepath)
     file_id = self:get_row_id(filepath)
   else
   -- ..or update existing entry
-    self:do_transaction('file_update_counter', { path = filepath })
+    self:do_transaction('file_update_counter', filepath)
   end
 
   -- register timestamp for this update
-  self:do_transaction('timestamp_add_entry', { file_id = file_id })
+  self:do_transaction('timestamp_add_entry', file_id)
 
   -- trim timestamps to MAX_TIMESTAMPS per file (there should be up to MAX_TS + 1 at this point)
-  local timestamps = self:do_transaction('get_timestamp_ids_for_file', { file_id = file_id })
+  local timestamps = self:do_transaction('get_timestamp_ids_for_file', file_id)
   local trim_at = timestamps[(#timestamps - MAX_TIMESTAMPS) + 1]
   if trim_at then
-    self:do_transaction('timestamp_delete_before_id', { id = trim_at.id, file_id = file_id })
+    self:do_transaction('timestamp_delete_before_id', { trim_at.id, file_id })
   end
 end
 
